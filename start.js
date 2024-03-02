4a04ccca952900c2dcfab2f842b307f85cf066a7
sankar-coding <raulsankar99@gmail.com>
2024-03-03 01:13:16 +0530

    🤯

--- start.js
+++ start.js
@@ -1,14 +1,14 @@
 const express = require("express")
 const app = express()
 const path = require("path")
-app.use(express.static("res"))
-
+app.use(express.static(path.join(__dirname,"res")))
+/*app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname,'res/404.html'));
+});*/
  app.get("/", (req,res) => {
     res.sendFile(path.join(__dirname, 'index.html'))
 })
-app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname,'404.html'))
-})
+
 app.listen(8080, () => {
     console.log("server running at the port no 8080")
 })
