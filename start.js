16bfb635a70ec81770384c8b147ed5c6fac7666b
sankar-coding <raulsankar99@gmail.com>
2024-03-03 00:59:04 +0530

    😉

--- start.js
+++ start.js
@@ -1,14 +1,14 @@
 const express = require("express")
-const app = express();
-const path = require("path");
-app.use(express.static("res"));
+const app = express()
+const path = require("path")
+app.use(express.static("res"))
 
  app.get("/", (req,res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
+    res.sendFile(path.join(__dirname, 'index.html'))
+})
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname,'res/404.html'));
-});
+  res.status(404).sendFile(path.join(__dirname,'404.html'))
+})
 app.listen(8080, () => {
-    console.log("server running at the port no 8080");
-});
+    console.log("server running at the port no 8080")
+})
