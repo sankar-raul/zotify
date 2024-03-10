65ca53584d4c7dffeb893398f2bb838deabfcaea
sankar-coding <raulsankar99@gmail.com>
2024-03-10 17:16:46 +0530

    😘

--- start.js
+++ start.js
@@ -2,14 +2,14 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-app.use(express.static(path.join(__dirname, "res")));
+app.use(express.static(path.join(__dirname, "docs")));
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, 'res', 'index.html'));
+    res.sendFile(path.join(__dirname, 'docs', 'index.html'));
 });
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'res', '404.html'));
+    res.status(404).sendFile(path.join(__dirname, 'docs', '404.html'));
 });
 
 const PORT = process.env.PORT || 8080;
